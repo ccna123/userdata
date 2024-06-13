9f2e0a133d8bc2b65da7d54f91b63f63f7af8bc1
THANH <95860402+ccna123@users.noreply.github.com>
2024-06-13 15:33:30 +0900

    update

--- userdata.sh
+++ userdata.sh
@@ -109,6 +109,7 @@ WantedBy=multi-user.target
 # -----config cloudwatch agent-----
 sudo yum install amazon-cloudwatch-agent -y
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-config-wizard
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a status
 /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json
 
 # -----start ssm session-----
